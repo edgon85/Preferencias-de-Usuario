abcc94a72f0a8e577bb6cf0a77bd5dad4ba0e5ea
edgon <edgon85@gmail.com>
2019-05-25 23:58:08 -0600

    Drawers - crear un menu lateral

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 A Flutter application.
 
+[1 - Inicio proyecto, preferencias de usuario](../../tree/d8543e843e00ba3135544894b83c2218baaf873d/)
+
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:preferencias_de_usuario_app/src/pages/settings_page.dart';
+
 
 class HomePage extends StatelessWidget {
-
   static final String routeName = 'home';
 
   @override
@@ -10,6 +11,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Preferencias de usuario'),
       ),
+      drawer: _crearMenu(context),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -19,6 +21,54 @@ class HomePage extends StatelessWidget {
           Divider(),
           Text("Nombre de usuario:"),
           Divider()
+        ],
+      ),
+    );
+  }
+
+  Widget _crearMenu(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/menu-img.jpg'),
+                    fit: BoxFit.cover)
+            ),
+          ),
+          
+          ListTile(
+            leading: Icon(Icons.home, color: Colors.blue,),
+            title: Text('Home'),
+            onTap: (){
+             return Navigator.pushNamed(context, HomePage.routeName);
+            },
+          ),
+
+
+          ListTile(
+            leading: Icon(Icons.party_mode, color: Colors.blue,),
+            title: Text('Party mode'),
+            onTap: (){},
+          ),
+
+
+          ListTile(
+            leading: Icon(Icons.pages, color: Colors.blue,),
+            title: Text('People'),
+            onTap: (){},
+          ),
+
+          ListTile(
+            leading: Icon(Icons.settings, color: Colors.blue,),
+            title: Text('Settings'),
+            onTap: (){
+              Navigator.pushReplacementNamed(context, SettingsPage.routeName);
+            },
+          )
         ],
       ),
     );
